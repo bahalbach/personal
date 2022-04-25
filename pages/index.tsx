2b6a4819ce73e9e6484fc15d5639cdd7bf421e05
bahalbach <bahalbach@gmail.com>
2022-04-25 17:56:47 +0800

    Move contact info to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,8 @@ import { AiFillGithub } from "react-icons/ai";
 import userData from "../constants/data";
 import About from "../components/About";
 import Projects from "../components/Projects";
+import { MdEmail } from "react-icons/md";
+import { AiFillPhone } from "react-icons/ai";
 
 const Home: NextPage = () => {
   return (
@@ -19,13 +21,28 @@ const Home: NextPage = () => {
       <Projects />
       {/* <Hero />
       <FavouriteProjects />
+  */}
       <div className="flex justify-center">
         <LinkButton
           icon={<AiFillGithub />}
           href={userData.socialLinks.github}
           text="My Github"
         />
-      </div> */}
+      </div> 
+      <div className="max-w-6xl mx-auto p-5 sm:p-10">
+        {/* contact methods */}
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row items-center gap-4 rounded-md border hover:border-blue-500 p-5 text-sm dark:bg-gray-900 bg-gray-100 dark:text-gray-200">
+            <MdEmail />
+            <span>{userData.contact.email}</span>
+          </div>
+          <div className="flex flex-row items-center gap-4 rounded-md border hover:border-blue-500 p-5 text-sm dark:bg-gray-900 bg-gray-100 dark:text-gray-200">
+            <AiFillPhone />
+            <span>{userData.contact.phone}</span>
+          </div>
+        </div>
+        
+      </div>
     </Layout>
   );
 };
