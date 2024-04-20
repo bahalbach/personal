68b77cff574e64d5e06e0cfc33022c3176cafd09
Ben Halbach <bahalbach@gmail.com>
2024-04-20 16:42:07 +0800

    move things, update packages

--- src/app/notes/_components/Note.tsx
+++ src/app/notes/_components/Note.tsx
@@ -3,27 +3,8 @@ import RenderMarkdown from "./RenderMarkdown";
 import Link from "next/link";
 import { cache } from "react";
 import { unstable_cache } from "next/cache";
+import { getGithubFileContent } from "../_utils/getGithubFileContent";
 
-const getGithubFileContent = unstable_cache(
-  cache(async (sha: string, label: string) => {
-    const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
-    const res = await octokit.rest.git.getBlob({
-      owner: "bahalbach",
-      repo: "notes",
-      file_sha: sha,
-      mediaType: {
-        format: "raw",
-      },
-    });
-    console.log(`fetch ${label}`);
-
-    // The type of res is incorrect, it's not taking in to
-    // account mediaType.format=raw
-    const text = res.data as unknown as string;
-    return text;
-  }),
-  ["github-markdown-page"]
-);
 export default async function Note({
   content,
   active,
---
+++ src/app/notes/_utils/getGithubFileContent.ts
@@ -0,0 +1,25 @@
+import { Octokit } from "octokit";
+import { cache } from "react";
+import { unstable_cache } from "next/cache";
+
+export const getGithubFileContent = unstable_cache(
+  cache(async (sha: string, label: string) => {
+    const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
+
+    const res = await octokit.rest.git.getBlob({
+      owner: "bahalbach",
+      repo: "notes",
+      file_sha: sha,
+      mediaType: {
+        format: "raw",
+      },
+    });
+    console.log(`fetch ${label}`);
+
+    // The type of res is incorrect, it's not taking in to
+    // account mediaType.format=raw
+    const text = res.data as unknown as string;
+    return text;
+  }),
+  ["github-markdown-page"]
+);
