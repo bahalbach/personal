bfa990afe95d9f1f2d9ddba563edcfc83235d555
Ben Halbach <bahalbach@gmail.com>
2024-05-03 13:31:04 +0800

    use cache around unstable_cache for getGithubFileContent

--- src/app/notes/utils/getGithubFileContent.ts
+++ src/app/notes/utils/getGithubFileContent.ts
@@ -2,24 +2,26 @@ import { Octokit } from "octokit";
 import { cache } from "react";
 import { unstable_cache } from "next/cache";
 
-export const getGithubFileContent = unstable_cache(
-  async (sha: string, label: string) => {
-    const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
+export const getGithubFileContent = cache(
+  unstable_cache(
+    async (sha: string, label: string) => {
+      const octokit = new Octokit({ auth: process.env.GITHUB_ACCESS_TOKEN });
 
-    const res = await octokit.rest.git.getBlob({
-      owner: "bahalbach",
-      repo: "notes",
-      file_sha: sha,
-      mediaType: {
-        format: "raw",
-      },
-    });
-    console.log(`fetch ${label}`);
+      const res = await octokit.rest.git.getBlob({
+        owner: "bahalbach",
+        repo: "notes",
+        file_sha: sha,
+        mediaType: {
+          format: "raw",
+        },
+      });
+      console.log(`fetch ${label}`);
 
-    // The type of res is incorrect, it's not taking in to
-    // account mediaType.format=raw
-    const text = res.data as unknown as string;
-    return text;
-  },
-  ["github-markdown-page"]
+      // The type of res is incorrect, it's not taking in to
+      // account mediaType.format=raw
+      const text = res.data as unknown as string;
+      return text;
+    },
+    ["github-markdown-page"]
+  )
 );
